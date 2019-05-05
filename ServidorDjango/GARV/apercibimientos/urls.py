d9296d6266c3e3ca54703cd1a2b70407b6675b96
MiguelMBP <miguelmbp98@gmail.com>
2019-05-05 13:28:40 +0200

    Informe resumen de apercibimientos por url

--- ServidorDjango/GARV/apercibimientos/urls.py
+++ ServidorDjango/GARV/apercibimientos/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('buscar/', views.buscarApercibimiento, name="buscarApercibimientos"),
     path('informe/informeNumeroApercibimiento/<int:anno>/<int:mes>/<str:unidad>/<int:minimo>', views.informeNumeroApercibimiento, name="informeNumeroApercibimiento"),
     path('informe/informeApercibimientoIndividual/<int:anno>/<int:mes>/<str:unidad>', views.informeApercibimientoIndividual, name="informeApercibimientoIndividual"),
+    path('informe/informeResumenApercibimiento/<int:anno>/<int:mes>/<str:unidad>', views.informeResumenApercibimiento, name="informeResumenApercibimiento"),
 ]
--- ServidorDjango/GARV/apercibimientos/views.py
+++ ServidorDjango/GARV/apercibimientos/views.py
@@ -4,6 +4,7 @@ import tempfile
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.db.models.functions import TruncMonth
 from django.shortcuts import render
 
 from django.http import HttpResponse
@@ -96,7 +97,7 @@ def informeNumeroApercibimiento(request, anno, mes, unidad, minimo):
     apercibimientos = Apercibimiento.objects.only("alumno", "unidad", "materia").filter(periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha], unidad=unidad).order_by("unidad", "alumno", "materia")
     lista = []
     for apercibimiento in apercibimientos:
-        resultado = Apercibimiento.objects.filter(alumno=apercibimiento.alumno, unidad=apercibimiento.unidad, materia=apercibimiento.materia, periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha]).count()
+        resultado = Apercibimiento.objects.filter(alumno=apercibimiento.alumno, unidad=apercibimiento.unidad, materia=apercibimiento.materia, periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha], activo=True).count() #recorre la lista de apercibimientos por cada alumno y materia
 
         if resultado >= minimo:
             apercibimiento = InformeNumero(alumno=apercibimiento.alumno, unidad=apercibimiento.unidad, materia=apercibimiento.materia, numero=resultado)
@@ -134,19 +135,19 @@ def informeApercibimientoIndividual(request, anno, mes, unidad):
     apercibimientos = Apercibimiento.objects.values("alumno", "unidad").distinct().filter(periodo_academico=anno, fecha_inicio__range=[fechainicio, fecha], unidad=unidad).order_by("unidad", "alumno") #todos los alumnos
     lista = []
     for apercibimiento in apercibimientos:
-        resultados = Apercibimiento.objects.filter(alumno=apercibimiento['alumno'], unidad=apercibimiento['unidad'], periodo_academico=anno, fecha_inicio__range=[fechainicio, fecha]) #todas las asignaturas de un alumno con apercibimiento en ese mes
+        resultados = Apercibimiento.objects.filter(alumno=apercibimiento['alumno'], unidad=apercibimiento['unidad'], periodo_academico=anno, fecha_inicio__range=[fechainicio, fecha], activo=True) #todas las asignaturas de un alumno con apercibimiento en ese mes
 
         listaAp = []
         for resultado in resultados:
             resultados2 = Apercibimiento.objects.filter(alumno=resultado.alumno, unidad=resultado.unidad, materia=resultado.materia, periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha])
             numero = len(resultados2)
             materia = resultado.materia
-            faltas = resultado.horas_injustificadas
+            faltas = resultado.horas_injustificadas[:-3]
             porcentaje = resultado.porcentaje_injustificado
 
             alumno = ApercibimientoIndividual(numero=numero, materia=materia, faltas=faltas, porcentaje=porcentaje)
-            if alumno not in listaAp:
-                listaAp.append(alumno)
+
+            listaAp.append(alumno)      #contiene cada apercibimiento de un alumno
 
         lista.append(InformeIndividual(alumno=apercibimiento['alumno'], unidad=apercibimiento['unidad'], fecha=sacarFecha(anno, mes), apercibimientos=listaAp))
 
@@ -156,6 +157,53 @@ def informeApercibimientoIndividual(request, anno, mes, unidad):
 
     response = HttpResponse(content_type='application/pdf;')
     response['Content-Disposition'] = 'inline; filename=informeApercibimientoIndividual.pdf'
+    response['Content-Transfer-Encoding'] = 'binary'
+
+    with tempfile.NamedTemporaryFile(delete=True) as output:
+        output.write(result)
+        output.flush()
+        output = open(output.name, 'rb')
+        response.write(output.read())
+
+    return response
+
+
+def informeResumenApercibimiento(request, anno, mes, unidad):
+    if 9 <= mes <= 12:
+        fecha = datetime.datetime(anno, mes, 28)
+    else:
+        fecha = datetime.datetime(anno + 1, mes, 28)
+
+    fechacurso = datetime.datetime(anno, 9, 1)
+    listacursos = []
+    if unidad == 'todos':
+        cursos = Apercibimiento.objects.values("unidad").distinct().filter(periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha]).order_by("unidad") #todos los cursos
+
+    else:
+        cursos = Apercibimiento.objects.values("unidad").distinct().filter(periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha], unidad=unidad).order_by("unidad") #curso por parametros
+
+    for curso in cursos:
+        lista = []
+        materias = Apercibimiento.objects.values("materia", "unidad").distinct().filter(periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha], unidad=curso['unidad']).order_by("unidad", "materia") #todas las materias
+
+        for materia in materias:
+            listamat = []
+            alumnos = Apercibimiento.objects.values("alumno").distinct().filter(materia=materia['materia'], unidad=materia['unidad'], periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha], activo=True) #todos los alumnos con apercibimiento en una asignatura
+
+            for alumno in alumnos:
+                resultado = Apercibimiento.objects.annotate(month=TruncMonth('fecha_inicio')).values('month').filter(alumno=alumno["alumno"], materia=materia['materia'], unidad=materia['unidad'], periodo_academico=anno, fecha_inicio__range=[fechacurso, fecha], activo=True).order_by('month')
+                listamat.append(ApercibimientoMateria(alumno=alumno["alumno"], numero=len(resultado), meses=listarMeses(resultado)))
+
+            lista.append(InformeResumen(materia=materia['materia'], alumnos=listamat))
+
+        listacursos.append(InformeResumenCurso(unidad=curso['unidad'], fecha=sacarFecha(anno, mes), materias=lista))
+
+    html_string = render_to_string('ResumenApercibimientosMateria.html', {'lista': listacursos})
+    html = HTML(string=html_string)
+    result = html.write_pdf()
+
+    response = HttpResponse(content_type='application/pdf;')
+    response['Content-Disposition'] = 'inline; filename=ResumenApercibimientosMateria.pdf'
     response['Content-Transfer-Encoding'] = 'binary'
 
     with tempfile.NamedTemporaryFile(delete=True) as output:
@@ -194,6 +242,17 @@ def sacarFecha(anno, mes):
         return 'Agosto de ' + str(anno+1)
 
 
+def listarMeses(resultados):
+    meses = ''
+    for resultado in resultados:
+        mes = resultado['month'].month
+        meses += str(mes) + ', '
+
+    return meses[:-2]
+
+
+
+#Clases para informe para 2 o mas apercibientos
 class InformeNumero:
     def __init__(self, alumno, unidad, materia, numero):
         self.alumno = alumno
@@ -212,6 +271,8 @@ class InformeNumero:
         return self.alumno == other.alumno  and self.unidad == other.unidad and self.materia == other.materia \
                and self.numero == other.numero
 
+
+#Clases para informe individual para alumnos
 class InformeIndividual:
     def __init__(self, alumno, unidad, fecha, apercibimientos):
         self.alumno = alumno
@@ -227,6 +288,7 @@ class InformeIndividual:
         return self.alumno == other.alumno and self.unidad == other.unidad and str(self.fecha) == str(other.fecha) \
                and self.apercibimientos == other.apercibimientos
 
+
 class ApercibimientoIndividual:
     def __init__(self, numero, materia, faltas, porcentaje):
         self.numero = numero
@@ -241,3 +303,25 @@ class ApercibimientoIndividual:
 
         return str(self.numero) == str(other.numero) and self.materia == other.materia and str(self.faltas) == str(other.faltas) \
                and str(self.porcentaje) == str(other.porcentaje)
+
+
+#Clases para informe de resumen de apercibimientps
+class InformeResumen:
+    def __init__(self, materia, alumnos):
+        self.materia = materia
+        self.alumnos = alumnos
+
+
+class InformeResumenCurso:
+    def __init__(self, unidad, fecha, materias):
+        self.fecha = fecha
+        self.unidad = unidad
+        self.materias = materias
+
+
+class ApercibimientoMateria:
+    def __init__(self, alumno, numero, meses):
+        self.alumno = alumno
+        self.numero = numero
+        self.meses = meses
+
